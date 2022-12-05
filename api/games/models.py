231a4ce717327c1899a96971f292534745a9354a
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-12-05 16:23:05 -0500

    add tic tac toe history

--- api/games/models.py
+++ api/games/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from users.models import MyUser as User
 
 # Create your models here.
 
@@ -11,3 +12,26 @@ class Game(models.Model):
 
   def __str__(self) -> str:
     return str(self.id) + ": " + self.title
+
+class Match(models.Model):
+  users = models.ManyToManyField(User, through='UserMatch')
+  game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="tic_tac_toe_matches")
+  game_settings = models.TextField(blank=True)
+  game_status = models.TextField(blank=True)
+  finished = models.BooleanField(default=False)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+class UserMatch(models.Model):
+  options = (("pending", "pending"), ("accepted", "accepted"), ("declined", "declined"))
+
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_matches")
+  friend = models.ForeignKey(User, on_delete=models.CASCADE)
+  invited_by = models.ForeignKey(User, on_delete=models.CASCADE)
+  match = models.ForeignKey(Match, on_delete=models.CASCADE, related_name="user_matches")
+  status = models.CharField(max_length=10, choices=options, default="pending")
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    unique_together = ("user", "match")
