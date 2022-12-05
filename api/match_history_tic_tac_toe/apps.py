c1d2e78437253c59eac4efc0cd3324e64f311ac6
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-12-05 16:13:59 -0500

    add match and userMatch models

---
+++ api/match_history_tic_tac_toe/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MatchHistoryTicTacToeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'match_history_tic_tac_toe'
